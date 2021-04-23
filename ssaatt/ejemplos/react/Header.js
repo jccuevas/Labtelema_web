759b6f0261036d419328b7c83e526a29f7dc5187
jccuevas <jccuevas@ujaen.es>
2021-04-23 13:17:02 +0200

    Añadiendo ejemplos React

---
+++ ssaatt/ejemplos/react/Header.js
@@ -0,0 +1,12 @@
+
+
+class Header extends React.Component {
+    render() {
+      return (
+        <header className="title">
+          <h1>¡Hola {this.props.person}! </h1>
+        </header>
+      );
+    }
+  }
+
---
+++ ssaatt/ejemplos/react/Main.js
@@ -0,0 +1,13 @@
+class Main extends React.Component {
+    render() {
+      return (
+        <main>
+          <h2>Esto es una elemento complejo</h2>
+          <p>
+            La complejidad de este elemento me debe hacer pensar que podría
+            extraer algún <strong>Componente</strong>
+          </p>
+        </main>
+      );
+    }
+  }
